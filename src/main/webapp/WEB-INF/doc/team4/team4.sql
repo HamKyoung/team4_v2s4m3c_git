373ad473f3d12d5ec7e2e7c6f439100eaca1f0e7
ryu <hm3mj0834@naver.com>
2021-02-24 11:49:46 +0900

    ERD 수정

--- src/main/webapp/WEB-INF/doc/team4/team4.sql
+++ src/main/webapp/WEB-INF/doc/team4/team4.sql
@@ -552,6 +552,78 @@ FROM (
 WHERE r >= 1 AND r <= 10;
 
 /**********************************/
+/* Table Name: 공채달력 */
+/**********************************/
+drop table re_calendar;
+CREATE TABLE re_calendar(
+		calendar_no                   		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		RECRUITNO                     		NUMBER(10)		 NULL ,
+		c_rdate                       		VARCHAR2(50)		 NOT NULL,
+		c_edate                       		VARCHAR2(50)		 NOT NULL,
+		c_title                       		VARCHAR2(300)		 NOT NULL,
+		c_cnt                         		NUMBER(10)		 DEFAULT 0		 NOT NULL,
+		c_date                        		DATE		 NOT NULL,
+  FOREIGN KEY (RECRUITNO) REFERENCES RECRUIT (RECRUITNO)
+);
+
+COMMENT ON TABLE re_calendar is '공채달력';
+COMMENT ON COLUMN re_calendar.calendar_no is '공채달력번호';
+COMMENT ON COLUMN re_calendar.RECRUITNO is '구인번호';
+COMMENT ON COLUMN re_calendar.c_rdate is '접수 시작일';
+COMMENT ON COLUMN re_calendar.c_edate is '접수 마감일';
+COMMENT ON COLUMN re_calendar.c_title is '제목';
+COMMENT ON COLUMN re_calendar.c_cnt is '조회수';
+COMMENT ON COLUMN re_calendar.c_date is '등록날짜';
+
+
+
+DROP SEQUENCE re_calendar_seq;
+CREATE SEQUENCE re_calendar_seq
+  START WITH 1              -- 시작 번호
+  INCREMENT BY 1          -- 증가값
+  MAXVALUE 9999999999 -- 최대값: 9999999 --> NUMBER(7) 대응
+  CACHE 2                       -- 2번은 메모리에서만 계산
+  NOCYCLE;                     -- 다시 1부터 생성되는 것을 방지
+
+  -- insert
+INSERT INTO re_calendar(calendar_no, RECRUITNO, c_rdate, c_edate, c_title, c_cnt, c_date)
+VALUES(re_calendar_seq.nextval, 1, '2021-02-11', '2021-03-11', '사람을 구합니다.', 0, sysdate);
+
+INSERT INTO re_calendar(calendar_no, RECRUITNO, c_rdate, c_edate, c_title, c_cnt, c_date)
+VALUES(re_calendar_seq.nextval, 1, '2021-03-11', '2021-04-11', '새로운 사람', 0, sysdate);
+
+INSERT INTO re_calendar(calendar_no, RECRUITNO, c_rdate, c_edate, c_title, c_cnt, c_date)
+VALUES(re_calendar_seq.nextval, 1, '2021-04-11', '2021-05-11', '가족같이 일 할 직원', 0, sysdate);
+
+-- list
+SELECT c.c_rdate, c.c_edate, c.c_title, c.RECRUITNO, ci.com_name
+FROM re_calendar c, RECRUIT re, COM_CATE cc, COM_INTRO ci
+WHERE re.RECRUITNO = c.RECRUITNO AND re.CATENO = cc.CATENO AND cc.comno = ci.comno
+ORDER BY re.RECRUITNO DESC;
+
+
+-- read, 조회
+SELECT calendar_no, RECRUITNO, c_rdate, c_edate, c_title, c_cnt, c_date
+FROM re_calendar
+WHERE calendar_no = 1;
+
+
+
+-- dalete, 삭제
+DELETE FROM re_calendar
+WHERE calendar_no = 4;
+
+
+-- update, 수정
+UPDATE re_calendar
+SET c_rdate='2021-02-20', c_edate='2021-02-21'
+WHERE calendar_no = 1;
+
+
+
+commit;
+
+/**********************************/
 /* Table Name: 첨부파일 */
 /**********************************/
 DROP TABLE attachfile;
@@ -733,6 +805,105 @@ WHERE p.comno = c.comno
 ORDER BY pass_self_no DESC;
 
 commit;
+
+/**********************************/
+/* Table Name: 합격자소서 첨부파일 */
+/**********************************/
+CREATE TABLE ps_attachfile(
+		ps_attachfileno               		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		pass_self_no                  		NUMBER(7)		 NOT NULL,
+		ps_fname                      		VARCHAR2(100)		 NOT NULL,
+		ps_upname                     		VARCHAR2(100)		 NOT NULL,
+		ps_thumb                      		VARCHAR2(100)		 NULL ,
+		ps_fsize                      		NUMBER(10)		 NOT NULL,
+		ps_rdate                      		DATE		 NOT NULL,
+  FOREIGN KEY (pass_self_no) REFERENCES pass_self (pass_self_no)
+);
+
+
+COMMENT ON TABLE ps_attachfile is '합격자소서 첨부파일';
+COMMENT ON COLUMN ps_attachfile.ps_attachfileno is '첨부파일 번호';
+COMMENT ON COLUMN ps_attachfile.pass_self_no is '합격자소서 번호';
+COMMENT ON COLUMN ps_attachfile.ps_fname is '원본 파일명';
+COMMENT ON COLUMN ps_attachfile.ps_upname is '업로드 파일명';
+COMMENT ON COLUMN ps_attachfile.ps_thumb is 'Thumb 파일명';
+COMMENT ON COLUMN ps_attachfile.ps_fsize is '파일 사이즈';
+COMMENT ON COLUMN ps_attachfile.ps_rdate is '등록일';
+
+
+DROP SEQUENCE ps_attachfile_seq;
+CREATE SEQUENCE ps_attachfile_seq
+  START WITH 1              -- 시작 번호
+  INCREMENT BY 1          -- 증가값
+  MAXVALUE 9999999999 -- 최대값: 9999999 --> NUMBER(7) 대응
+  CACHE 2                       -- 2번은 메모리에서만 계산
+  NOCYCLE;                     -- 다시 1부터 생성되는 것을 방지
+
+commit;
+
+
+-- 1) 등록
+INSERT INTO ps_attachfile(ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate)
+VALUES(ps_attachfile_seq.nextval, 3, 'samyang.jpg', 'samyang_1.jpg', 'samyang_t.jpg', 1000, sysdate);
+
+INSERT INTO ps_attachfile(ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate)
+VALUES(ps_attachfile_seq.nextval, 1, 'samyang2.jpg', 'samyang2_1.jpg', 'samyang2_t.jpg', 2000, sysdate);
+             
+INSERT INTO ps_attachfile(ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate)
+VALUES(ps_attachfile_seq.nextval,  1, 'samyang3.jpg', 'samyang3_1.jpg', 'samyang3_t.jpg', 3000, sysdate);     
+
+
+-- 2) 목록( pass_self_no 기준 내림 차순, ps_attachfileno 기준 오름차순)
+SELECT ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate
+FROM ps_attachfile
+ORDER BY pass_self_no DESC,  ps_attachfileno ASC;
+
+
+-- 3) 글별 파일 목록(pass_self_no 기준 내림 차순, ps_attachfileno 기준 오르차순)
+SELECT ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate
+FROM ps_attachfile
+WHERE pass_self_no = 1
+ORDER BY ps_attachfileno ASC;
+
+
+SELECT ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate
+FROM ps_attachfile
+WHERE ps_attachfileno=10;
+
+
+-- 4) 하나의 파일 삭제
+DELETE FROM ps_attachfile
+WHERE ps_attachfileno = 9;
+
+
+-- 5) FK pass_self_no 부모키 별 조회
+SELECT ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate
+FROM ps_attachfile
+WHERE pass_self_no=1;
+
+
+-- 부모키별 갯수 산출
+SELECT COUNT(*) as cnt
+FROM ps_attachfile
+WHERE pass_self_no=1;
+
+
+-- 6) FK 부모 테이블별 레코드 삭제
+DELETE FROM ps_attachfile
+WHERE pass_self_no=1;
+
+
+-- 7) pass_self, ps_attachfile join
+    SELECT c.pass_self_title, a.ps_attachfileno, a.pass_self_no, a.ps_fname, a.ps_upname, a.ps_thumb, a.ps_fsize, a.ps_rdate
+    FROM pass_self c, ps_attachfile a
+    WHERE c.pass_self_no = a.pass_self_no
+    ORDER BY c.pass_self_no DESC,  a.ps_attachfileno ASC;
+
+
+-- 8) 조회
+SELECT ps_attachfileno, pass_self_no, ps_fname, ps_upname, ps_thumb, ps_fsize, ps_rdate
+FROM ps_attachfile
+WHERE ps_attachfileno=8;
 
 
 /**********************************/
@@ -1164,4 +1335,100 @@ VALUES(com_review_seq.nextval, '제일기획', '제목', '5', '데이터마케팅', '밥이 맛
 SELECT*FROM com_review ORDER BY rev_no ASC;
 
 commit;
+
+DROP TABLE event CASCADE CONSTRAINTS;
+
+/**********************************/
+/* Table Name: 이벤트 */
+/**********************************/
+CREATE TABLE event(
+		eventno                       		NUMBER(10)		 NOT NULL		 PRIMARY KEY,
+		eve_title                     		VARCHAR2(80)		 NOT NULL,
+		eve_content                   		VARCHAR2(800)		 NOT NULL,
+		eve_visible                   		CHAR(1)		 NOT NULL,
+		eve_date                      		DATE		 NOT NULL,
+		eve_pic                       		VARCHAR2(500)		 NULL ,
+		eve_thumb                     		VARCHAR2(100)		 NULL ,
+		eve_size                      		NUMBER(10)		 NULL 
+);
+
+COMMENT ON TABLE event is '이벤트';
+COMMENT ON COLUMN event.eventno is '이벤트 번호';
+COMMENT ON COLUMN event.eve_title is '이벤트 제목';
+COMMENT ON COLUMN event.eve_content is '이벤트 내용';
+COMMENT ON COLUMN event.eve_visible is '공개 여부';
+COMMENT ON COLUMN event.eve_date is '작성일';
+COMMENT ON COLUMN event.eve_pic is '사진';
+COMMENT ON COLUMN event.eve_thumb is '썸네일';
+COMMENT ON COLUMN event.eve_size is '사진 용량';
+
+
+DROP SEQUENCE event_seq;
+CREATE SEQUENCE event_seq
+  START WITH 1              -- 시작 번호
+  INCREMENT BY 1          -- 증가값
+  MAXVALUE 9999999999 -- 최대값: 9999999 --> NUMBER(7) 대응
+  CACHE 2                       -- 2번은 메모리에서만 계산
+  NOCYCLE;                     -- 다시 1부터 생성되는 것을 방지
+  
+  
+-- 등록
+INSERT INTO event(eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size)
+VALUES(event_seq.nextval, '이벤트 제목', '이벤트 내용입니다. 기간: 2021.01.27~2021.12.31 이벤트를 개최합니다. 응모시 추첨을 통하여 상품을 드립니다.', 'Y', sysdate, '사진', '사진_t', 1); 
+  
+--목록
+SELECT * FROM event
+ORDER BY eventno ASC;
+
+
+-- 검색된 레코드 갯수
+SELECT COUNT(*) as cnt
+FROM event
+WHERE eve_title LIKE '%제목%';
+
+-- step 3, 1 page
+SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size, r
+FROM (
+           SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size, rownum as r
+           FROM (
+                     SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size
+                     FROM event
+                     WHERE eve_title LIKE '%제목%'
+                     ORDER BY eventno DESC
+           )          
+)
+WHERE r >= 1 AND r <= 10;
+
+-- step 3, 2 page
+SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size, r
+FROM (
+           SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size, rownum as r
+           FROM (
+                     SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size
+                     FROM event
+                     WHERE eve_title LIKE '%제목%'
+                     ORDER BY eventno DESC
+           )          
+)WHERE r >= 11 AND r <= 20;
+
+-- read, 조회
+SELECT eventno,eve_title,eve_content,eve_visible,eve_date,eve_pic,eve_thumb,eve_size
+FROM event
+WHERE eventno = 1;
+
+-- update, 수정
+UPDATE event
+SET eve_title='수정 완료', eve_content='수정 완료'
+WHERE eventno = 1;
+
+-- dalete, 삭제
+DELETE FROM event
+WHERE eventno = 3;
+
+-- 출력 모드의 변경
+UPDATE event
+SET eve_visible='N'
+WHERE eventno=1;
+
+COMMIT;  
 
